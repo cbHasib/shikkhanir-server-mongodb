da18984edeb7992165bd2ce9619859048beb82ca
cbHasib <hasibul.pbn@gmail.com>
2022-11-02 12:21:48 +0600

    Deploy

--- index.js
+++ index.js
@@ -8,9 +8,17 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("ShikkhaNir Server is Running");
+});
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pkcv1zd.mongodb.net/?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
   try {
@@ -20,7 +28,7 @@ async function run() {
     console.log("Connection error!", error.name, error.message);
   }
 }
-run();
+run().catch((err) => console.log(err));
 
 // Database on MongoDB
 const db = client.db(`${process.env.DB_NAME}`);
